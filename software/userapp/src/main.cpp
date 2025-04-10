92d2bdee4f1112990e15a2c147fed7b19d735455
Sergei Khabarov <sergei.khabarov@qdevtechnologies.com>
2025-04-10 23:56:36 +0300

    Add some basic userapp to mmap BAR0 of the /dev/khbr_accel device

---
+++ software/userapp/src/main.cpp
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2025 Sergey Khabarov, sergeykhbr@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>   // mmap()
+#include <fcntl.h>
+#include <stdio.h>
+#include "apptypes.h"
+
+int main(int argc, char *argv[])
+{
+    FrameBufferType *fb;
+    int fd = open("/dev/khbr_accel", O_RDWR);
+
+    if (!fd) {
+        printf("Cannot open device /dev/khbr_accel\n");
+        return -1;
+    }
+
+
+    fb = (FrameBufferType *)mmap(NULL,                     // addr
+                                 sizeof(FrameBufferType),  // length
+                                 PROT_READ | PROT_WRITE,   // prot
+                                 MAP_SHARED,               // flags
+                                 fd,                       // fd
+                                 0);                       // offset
+    if (fb == MAP_FAILED) {
+        printf("Failed mmap()\n");
+        close(fd);
+        return -1;
+    }
+
+    /**
+      Basic read/write tests.
+     */
+
+    fb.ui64[0] = 0xddccbbaa44332211ull;
+    fb.ui64[1] = 0x123456789abcdef0ull;
+    if (fb.ui32[0] != 0x44332211) {
+        printf("Wrong fb.ui32[0]=%08x\n", fb.ui32[0]);
+    }
+    if (fb.ui32[1] != 0xddccbbaa) {
+        printf("Wrong fb.ui32[1]=%08x\n", fb.ui32[1]);
+    }
+    if (fb.ui64[1] != 0x123456789abcdef0ull) {
+        printf("Wrong fb.ui64[1]=%016llx\n", fb.ui64[1]);
+    }
+    printf("mmap() tests finished\n");
+    munmap(fb, sizeof(FrameBufferType));
+
+    char rbuf[16];
+    read(fd, rbuf, 16);
+    printf("read() rbuf[0]=%08x\n", ((uint32_t *)rbuf)[0]);
+    printf("read() rbuf[1]=%08x\n", ((uint32_t *)rbuf)[1]);
+    printf("read() rbuf[3]=%08x\n", ((uint32_t *)rbuf)[2]);
+    printf("read() rbuf[4]=%08x\n", ((uint32_t *)rbuf)[3]);
+
+    close(fd);
+    return 0;
+}
