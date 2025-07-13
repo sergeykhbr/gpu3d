bff1fc29c37d5b88819819631cd9884334c5708e
sergeykhbr <sergeykhbr@gmail.com>
2025-07-13 16:16:21 +0300

    [!] Fix kc705 rtl simulation makefiles

--- prj/common/lists/cdc.f
+++ prj/common/lists/cdc.f
@@ -2,3 +2,4 @@ ${RTL_HOME}/internal/cdc/cdc_afifo_gray.sv
 ${RTL_HOME}/internal/cdc/cdc_dp_mem.sv
 ${RTL_HOME}/internal/cdc/cdc_afifo.sv
 ${RTL_HOME}/internal/cdc/afifo_xslv.sv
+${RTL_HOME}/internal/cdc/afifo_xmst.sv
